c26ae9aa97e13bf8fbd26cbf51db1e385c6dba12
Paweł Pela <paulpela@gmail.com>
2017-01-03 17:30:31 +0100

    formatting

--- boot.asm
+++ boot.asm
@@ -1,23 +1,30 @@
+; Mithrandir, Mithrandir, A Randir Vithren
+; ú-reniathach i amar galen
+; I reniad lín ne mór, nuithannen
+; In gwidh ristennin, i fae narchannen
+; I lach Anor ed ardhon gwannen
+; Caled veleg, ethuiannen.
+
 USE16
 ORG 0x7C00
 
 boot_stage_one:
-    mov ah, 0x00
-    mov dl, 0
-    int 0x13
-    mov ah, 0x02
-    mov al, 0x40 ; # of sectors to read (sectors are 512B)
-    mov dl, 0
-    mov ch, 0
-    mov dh, 0
-    mov cl, 2
-    mov bx, 0x8000
-    int 0x13
-    jmp 0x8000
+  mov ah, 0x00
+  mov dl, 0
+  int 0x13
+  mov ah, 0x02
+  mov al, 0x40 ; # of sectors to read (sectors are 512B)
+  mov dl, 0
+  mov ch, 0
+  mov dh, 0
+  mov cl, 2
+  mov bx, 0x8000
+  int 0x13
+  jmp 0x8000
 
 pad_boot_sector:
-    times ((0x200 - 2) - ($ - $$)) db 0x00
+  times ((0x200 - 2) - ($ - $$)) db 0x00
 
 boot_sector_magic_signeture:
-    dw 0xAA55
+  dw 0xAA55
 
